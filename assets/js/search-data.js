6420799dd3d7197c890565221a359f72b425a8ec
redwanulsourav <redwanul.sourav@gmail.com>
2026-02-16 02:50:19 +0000

    Deploying to gh-pages from @ redwanulsourav/redwanulsourav.github.io@9bde2ee0833a7e571e1b14fad60c7d1e54760c71 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-my-pr-to-pytorch-cpuinfo-was-merged",
+          title: 'My PR to pytorch/cpuinfo was merged!.',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
