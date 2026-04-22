9364d1f8d30763d73955cfe570aa3cbe2e5932d9
redwanulsourav <redwanul.sourav@gmail.com>
2026-04-22 04:59:38 +0000

    Deploying to gh-pages from @ redwanulsourav/redwanulsourav.github.io@721501d3acc6148ee000c75e8fc4590d19f2089c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -106,8 +106,8 @@ ninja.data = [{
             },},{id: "news-my-pr-was-merged-in-pytorch-cpuinfo",
           title: 'My PR was merged in pytorch/cpuinfo!',
           description: "",
-          section: "News",},{id: "news-i-will-be-joining-meta-as-phd-software-engineer-ml-for-summer-2026",
-          title: 'I will be joining Meta as PhD Software Engineer (ML) for Summer 2026!...',
+          section: "News",},{id: "news-i-will-be-joining-meta-as-phd-software-engineer-ml-intern-for-summer-2026",
+          title: 'I will be joining Meta as PhD Software Engineer (ML) Intern for Summer...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
@@ -159,14 +159,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%72%65%64%77%61%6E%75%6C.%73%6F%75%72%61%76@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-rss',
@@ -174,20 +167,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
